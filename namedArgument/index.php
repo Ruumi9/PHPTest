90279bf9306a88385e79ba2cc653b6c289d09419
Mat <asepskizo27@gmail.com>
2025-07-09 15:11:28 +0700

    namedArgument PHP

---
+++ namedArgument/index.php
@@ -0,0 +1,11 @@
+<?php
+
+function testNama($nama, $umur, $gender){
+    echo "Nama : $nama";
+    echo "Umur : $umur";
+    echo "Gender : $gender";
+    return;
+}
+// echo phpinfo();
+
+testNama(umur: 19, nama: "Rahmat", gender: "Pria");
